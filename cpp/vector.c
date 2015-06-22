8f9572f53911015b1c7088fc4aff7fca9a487b4f
Chris Richardson <crichardson332@gmail.com>
2015-06-21 22:40:44 -0700

    Added Vector3 struct to vector library

--- cpp/vector.c
+++ cpp/vector.c
@@ -2,7 +2,7 @@
 * File Name: vector.c
 * Purpose:
 * Creation Date: 20-06-2015
-* Last Modified: Sat Jun 20 22:00:45 2015
+* Last Modified: Sun Jun 21 22:39:05 2015
 * Created By: Chris Richardson
 *******************************************/
 
@@ -14,7 +14,7 @@
 void vector_init(Vector *vector) {
   vector->size = 0;
   vector->capacity = VECTOR_INITIAL_CAPACITY;
-  vector->data = malloc(sizeof(size_t) * vector->capacity);
+  vector->data = malloc(sizeof(double) * vector->capacity);
 }
 
 void vector_append(Vector *vector, double value) {
@@ -59,4 +59,44 @@ void vector_double_capacity(Vector *vector) {
 
 void vector_free(Vector *vector) {
   free(vector->data);
+}
+
+/* 
+ * Functions for Vector3 struct type
+ *
+ */
+void vector3_init(Vector3 *vector) {
+  vector->data = calloc(3, sizeof(double));
+}
+
+void vector3_free(Vector3 *vector) {
+  free(vector->data);
+}
+
+double vector3_dot(Vector3 *vecA, Vector3 *vecB) {
+  double dot_product;
+  dot_product = vecA->data[0] * vecB->data[0]
+    + vecA->data[1] * vecB->data[1]
+    + vecA->data[2] * vecB->data[2];
+  return dot_product;
+}
+
+double vector3_magnitude(Vector3 *vector) {
+  double mag;
+  mag = sqrt(pow(vector->data[0],2)
+    + pow(vector->data[1],2)
+    + pow(vector->data[2],2));
+  return mag;
+}
+
+Vector3 *vector3_cross(Vector3 *vecA, Vector3 *vecB) {
+  Vector3 *vecC;
+  vector3_init(vecC); 
+  vecC->data[0] = vecA->data[1] * vecB->data[2]
+                 -vecA->data[2] * vecB->data[1];
+  vecC->data[1] = vecA->data[2] * vecB->data[0]
+                 -vecA->data[0] * vecB->data[2];
+  vecC->data[2] = vecA->data[0] * vecB->data[1]
+                 -vecA->data[1] * vecB->data[0];
+  return vecC;
 }
--- cpp/vector.h
+++ cpp/vector.h
@@ -2,7 +2,7 @@
 * File Name: vector.h
 * Purpose:
 * Creation Date: 20-06-2015
-* Last Modified: Sat Jun 20 21:49:40 2015
+* Last Modified: Sun Jun 21 22:37:18 2015
 * Created By: Chris Richardson
 *******************************************/
 
@@ -11,6 +11,7 @@
 #define VECTOR_INITIAL_CAPACITY 100
 
 #include <stddef.h>
+#include <math.h>
 
 typedef struct {
   size_t size;     /* slots used so far */
@@ -24,5 +25,17 @@ double vector_get(Vector *vector, size_t index);
 void vector_set(Vector *vector, size_t index, double value);
 void vector_double_capacity(Vector *vector);
 void vector_free(Vector *vector);
+
+/* structure and functions for 3-vector */
+
+typedef struct {
+  double *data;
+} Vector3;
+
+void vector3_init(Vector3 *vector);
+void vector3_free(Vector3 *vector);
+double vector3_dot(Vector3 *vecA, Vector3 *vecB);
+double vector3_magnitude(Vector3 *vector);
+Vector3 *vector3_cross(Vector3 *vecA, Vector3 *vecB);
 
 #endif
