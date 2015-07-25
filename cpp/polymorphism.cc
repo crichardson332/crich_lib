2c0600330c08b76a12e67c41bcc201ddac9059f1
Chris Richardson <crichardson332@gmail.com>
2015-07-25 16:58:14 -0700

    Added polymorphism example file

--- cpp/polymorphism.cc
+++ cpp/polymorphism.cc
@@ -11,23 +11,23 @@ using namespace std;
 class Polygon {
   protected:
     int width, height;
+
   public:
-    void set_values (int a, int b)
-      { width=a; height=b; }
-    virtual int area ()
-      { return 0; }
+    void set_values (int a, int b) {width=a; height=b;}
+    virtual int area () {return 0;}
+    virtual void tryThis() {cout << "ORIGINAL" << endl;}
 };
 
 class Rectangle: public Polygon {
   public:
-    int area ()
-      { return width * height; }
+    int area () {return width * height;}
+    void tryThis() {cout << "OVERWRITTEN - RECT" << endl;}
 };
 
 class Triangle: public Polygon {
   public:
-    int area ()
-      { return (width * height / 2); }
+    int area () {return (width * height / 2);}
+    void tryThis() {cout << "OVERWRITTEN - TRGL" << endl;}
 };
 
 int main () {
@@ -40,8 +40,14 @@ int main () {
   ppoly1->set_values (4,5);
   ppoly2->set_values (4,5);
   ppoly3->set_values (4,5);
-  cout << ppoly1->area() << '\n';
-  cout << ppoly2->area() << '\n';
-  cout << ppoly3->area() << '\n';
+  //cout << ppoly1->area() << '\n';
+  //cout << ppoly2->area() << '\n';
+  //cout << ppoly3->area() << '\n';
+  cout << "Testing virtual func with polymorphism - rect: " << endl;
+  ppoly1->tryThis();
+  cout << "Testing virtual func with polymorphism - trgl: " << endl;
+  ppoly2->tryThis();
+  cout << "Testing virtual func with polymorphism - poly: " << endl;
+  ppoly3->tryThis();
   return 0;
 }
