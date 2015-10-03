3ed3cb11dbb36a137e07528744e46a608be49a8a
Chris Richardson <crichardson332@gmail.com>
2015-10-03 19:53:18 -0400

    added more to SimODE class

--- cpp/SimODE.cc
+++ cpp/SimODE.cc
@@ -7,6 +7,7 @@
 * Created By: Chris Richardson
 *******************************************/
 #include "SimODE.h"
+#include "assert.h"
 #include <iostream>
 
 double thetaDot(double theta) {
@@ -17,8 +18,38 @@ void SimODE::setIntegratorType(IntegType integ) {
     integrator_type = integ;
 }
 
+void SimODE::setInitialTime(double init_time) {
+    assert(init_time >= 0);
+    initial_time = init_time;
+}
+    
 void SimODE::setInitialState(std::vector<double> init_state) {
     initial_state = init_state;
 }
 
+void SimODE::setStepSize(double st_size) {
+    step_size = st_size;
+}
 
+void SimODE::reset() {
+    current_time = initial_time;
+    current_state = initial_state;
+} 
+
+void SimODE::step() {
+    switch (integrator_type) {
+        case euler: {
+            //do euler integration
+            std::vector<double> deriv {0,0}; 
+            for (int i=0;i<current_state.size();i++) {
+                current_state[i] += step_size * deriv[i];
+            }
+            current_time += step_size;
+            break;
+        }
+        case rk4: {
+            double temp = 0;
+            break;
+        }
+    }
+}
--- cpp/SimODE.h
+++ cpp/SimODE.h
@@ -14,14 +14,17 @@ class SimODE {
         double step_size;
         double initial_time;
         double (*deriv_func)(double);
-        double current_state;
         double current_time;
+        std::vector<double> current_state;
         std::vector<double> initial_state;
         IntegType integrator_type;
 
     public:
         void setIntegratorType(IntegType);
+        void setInitialTime(double);
         void setInitialState(std::vector<double>);
+        void setStepSize(double);
+        void reset();
         void step();
 };
         
